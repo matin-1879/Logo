d5ab97cc80c1a40539920e1f8e3e742afc7f84b8
M A T I N 1879 <87310379+matin-1879@users.noreply.github.com>
2021-07-18 13:50:39 +0300

    Create logo.sh

---
+++ logo.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+echo ""
+read -p $' \e[0m\e[1;92m Input your Username : \e[0m\e[1;96m\en' user
+echo ""
+echo ""
+echo -e  $g "Please  Wait  A few Minutes...... "
+echo ""
+apt update 
+apt upgrade -y 
+pkg install figlet -y
+pkg install ncurses-utils -y 
+pkg install ruby -y 
+gem install lolcat 
+figlet -f big WoW | lolcat
+git clone https://github.com/James404-cyber/Termux.git
+sed 's+CODEBAZ+'$user'+g' Termux/bash.bashrc > /data/data/com.termux/files/usr/etc/bash.bashrc
+sed 's+CODEBAZ+'$user'+g' Termux/logo.py > /data/data/com.termux/files/usr/etc/logo.py
+rm -rf Termux
+figlet -f big CODEBAZ | lolcat
